9b84fe817366c4e978ff8222523ede85f88a09be
Thiago Silva <itm_tsilva@uolinc.com>
2023-02-14 07:51:37 -0300

    organizando diretorio

--- src/domain/customer/factory/customer.validator.factory.ts
+++
@@ -1,9 +0,0 @@
-import ValidatorInterface from "../../@shared/validator/validator.interface";
-import Customer from "../entity/customer";
-import CustomerYupValidator from "../validator/customer.yup.validator";
-
-export default class CustomerValidatorFactory {
-  static create(): ValidatorInterface<Customer> {
-    return new CustomerYupValidator();
-  }
-}
