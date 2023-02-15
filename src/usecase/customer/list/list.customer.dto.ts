bf83111ad4433b81bc250e5f30f00638988b30ca
Thiago Silva <itm_tsilva@uolinc.com>
2023-02-15 08:33:37 -0300

    feat: list
    adicionado metodo para listagem de customers

---
+++ src/usecase/customer/list/list.customer.dto.ts
@@ -0,0 +1,14 @@
+interface Customer {
+  id: string;
+  name: string;
+  address: {
+    street: string;
+    city: string;
+    number: number;
+    zip: string;
+  };
+}
+
+export interface OutputListCustomerDto {
+  customers: Customer[];
+}
