6af5b897a23e45f1445a2d6339d8a994fa6b791f
Thiago Silva <itm_tsilva@uolinc.com>
2023-03-22 11:50:13 -0300

    refactor: alterados os tests para aceitar strings ao inves de array

--- src/domain/@shared/notification/notification.spec.ts
+++ src/domain/@shared/notification/notification.spec.ts
@@ -13,20 +13,17 @@ describe("Unit test for notifications", () => {
       message: "error message",
       context: "customer",
     });
-    expect(notification.message()).toEqual(["customer: error message"]);
+    expect(notification.message()).toEqual("customer: error message");
 
     notification.addError({ message: "error message 2", context: "customer" });
-    expect(notification.message()).toEqual([
-      "customer: error message",
-      "customer: error message 2",
-    ]);
+    expect(notification.message()).toEqual(
+      "customer: error message, customer: error message 2"
+    );
 
     notification.addError({ message: "error message 3", context: "order" });
-    expect(notification.message()).toEqual([
-      "customer: error message",
-      "customer: error message 2",
-      "order: error message 3",
-    ]);
+    expect(notification.message()).toEqual(
+      "customer: error message, customer: error message 2, order: error message 3"
+    );
   });
 
   it("should filter error with context", () => {
@@ -36,17 +33,27 @@ describe("Unit test for notifications", () => {
     notification.addError({ message: "error message 2", context: "customer" });
     notification.addError({ message: "error message 3", context: "order" });
 
-    expect(notification.message("customer")).toEqual([
-      "customer: error message",
-      "customer: error message 2",
-    ]);
+    expect(notification.message("customer")).toEqual(
+      "customer: error message, customer: error message 2"
+    );
 
-    expect(notification.message("order")).toEqual(["order: error message 3"]);
+    expect(notification.message("order")).toEqual("order: error message 3");
   });
 
   it("should return empty array when context not found", () => {
     const notification = new Notification();
     notification.addError({ message: "error message 3", context: "order" });
-    expect(notification.message("fake")).toEqual([]);
+    expect(notification.message("fake")).toEqual("");
+  });
+
+  it("should return true when has error message", () => {
+    const notification = new Notification();
+    notification.addError({ message: "error message 3", context: "order" });
+    expect(notification.hasErrors()).toBeTruthy();
+  });
+
+  it("should return false when no has error message", () => {
+    const notification = new Notification();
+    expect(notification.hasErrors()).toBeFalsy();
   });
 });
