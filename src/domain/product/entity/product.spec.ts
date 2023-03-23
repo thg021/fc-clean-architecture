8258e640b185a2a0eded8d084b6c411a21e00fed
thiago da silva <thg021@gmail.com>
2023-03-22 22:03:27 -0300

    test: adicionado teste do pattern em products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,22 +1,35 @@
+import { NotificationError } from "../../@shared/notification/notification.error";
 import Product from "./product";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      new Product("", "Product 1", 100);
+    }).toThrowError(new NotificationError("product: Id is required"));
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      new Product("123", "", 100);
+    }).toThrowError(new NotificationError("product: Name is required"));
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+      new Product("123", "Name", -1);
+    }).toThrowError(
+      new NotificationError("product: Price must be greater than zero")
+    );
+  });
+
+  it("should throw error when price, name and id is invalid", () => {
+    expect(() => {
+      new Product("", "", -1);
+    }).toThrowError(
+      new NotificationError(
+        "product: Id is required, product: Name is required, product: Price must be greater than zero"
+      )
+    );
   });
 
   it("should change name", () => {
